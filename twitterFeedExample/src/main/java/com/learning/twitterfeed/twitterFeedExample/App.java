1d44ba524b9cf83224851e6d57c78af4b4f24167
vsr426 <vsr426@gmail.com>
2019-03-01 15:30:06 -0500

    Add files via upload

--- twitterFeedExample/src/main/java/com/learning/twitterfeed/twitterFeedExample/App.java
+++ twitterFeedExample/src/main/java/com/learning/twitterfeed/twitterFeedExample/App.java
@@ -3,7 +3,9 @@ package com.learning.twitterfeed.twitterFeedExample;
 import java.util.ArrayList;
 import java.util.List;
 
+import twitter4j.GeoLocation;
 import twitter4j.Query;
+import twitter4j.Query.Unit;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -12,11 +14,11 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class App 
 {
-	private static String CONSUMERKEY="xxx";
-	private static String CONSUMERKEY_SECRET="xxx";
-	private static String ACCESSTOKEN="xxxx";
-	private static String ACCESSTOKEN_SECRET="xxxx";
-
+	private static String CONSUMERKEY="kEOtSXvW9obUyOtAVa5qF2HOB";
+	private static String CONSUMERKEY_SECRET="Nyki4IUD3pVqRR5dJEHFKErWNv1LJpdVVYUlDwHcerRhfjyaXm";
+	private static String ACCESSTOKEN="1081024273245003776-WcqxxWOLOTU6Oi2MfMAPNXzJkCsGoM";
+	private static String ACCESSTOKEN_SECRET="p351nNGsrWM1e7tPJefSRvRRWDOE8GSiTEMOJ5J3IEkfQ";
+	
     public static void main( String[] args )
     {
     	List<Status> responseStatuses= new ArrayList<Status>();
@@ -28,8 +30,11 @@ public class App
     	.setDebugEnabled(true)
     	.setJSONStoreEnabled(true)
     	.setTweetModeExtended(true);
-    	Query query = new Query("Bigdata, DataScience, MachineLearning");
+    	Query query = new Query("Job AND {bigdata OR apache spark OR hive}");
     	query.setCount(100);
+    	query.setLocale("en-US");
+    	
+//    	query.setGeoCode(new GeoLocation(42.3601, 71.0589), 500, Unit.mi);
     	Twitter twitter = new TwitterFactory(cb.build()).getInstance();
     	
     	try {
